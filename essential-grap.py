6403825dcccac8dce45bb9389fd8fde934c2f5c4
NOWA | SHINOBI <50762464+ArterHub@users.noreply.github.com>
2022-07-22 13:23:26 +0700

    Update essential-grap.py

--- essential-grap.py
+++ essential-grap.py
@@ -1,2 +1,11 @@
-if token == random.randint(1000, 9999) :
-  print("True")
+root = tk.Tk()
+root.geometry(300x650)
+root.title("Essential - GrapX")
+
+lb1 = tk.Label(root, text="Test")
+lb1.pack(pady=10)
+
+button1 = tk.Button(root, text="Clicked!",command: lambda: [WhenClick_Function()])
+
+
+root.mainloop()
