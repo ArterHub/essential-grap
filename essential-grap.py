437bc829157515fa201f18bbdebcada604b0a910
NOWA | SHINOBI <50762464+ArterHub@users.noreply.github.com>
2022-07-22 13:04:32 +0700

    Update essential-grap.py

--- essential-grap.py
+++ essential-grap.py
@@ -1,18 +1,2 @@
-import os
-import random
-import smtplib, ssl
-import string
-
-port = 465  # For SSL
-essential_grapx_server = "essential-grapx.gmail.com"
-sender_email = "gzamemsatergamer@gmail.com"  # Enter your address
-receiver_email = input("Your Email : ")  # Enter receiver address
-password = input("Type your password and press enter: ")
-message = f"""
-{random.radient(1000, 9999)}
-"""
-
-context = ssl.create_default_context()
-with smtplib.SMTP_SSL(essential_grapx_server, port, context=context) as server:
-    server.login(sender_email, password)
-    server.sendmail(sender_email, receiver_email, message)
+def verify(verify_code):
+    verify_code.randint(1000, 9999)
